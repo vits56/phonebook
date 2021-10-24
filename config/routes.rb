a013785b25d170ef85e47eaed3bf9dbc2a536561
Bruno Bastos <bastosduartebruno@gmail.com>
2021-10-24 19:26:16 -0300

    routes friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :friends
   #get 'home/index'
   get 'home/about'
   root 'home#index'
-  resources :friends
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
